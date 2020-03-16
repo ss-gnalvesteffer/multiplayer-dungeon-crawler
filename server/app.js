cbbf9e76ac6db844898274864841bdd7380cee05
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-03-15 20:45:25 -0500

    add manifest json

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('assets'));
-app.use(express.static(path.join(__dirname, '..', 'client')));
 
 io.on('connection', function(socket){
   console.log('a user connected');
