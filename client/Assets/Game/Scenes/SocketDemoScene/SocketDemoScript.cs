8cd8414f810d141f87c8a64b1eee51ecd466bc56
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-03-17 01:15:50 -0500

    added socket demo! web socket communication!!!!

---
+++ client/Assets/Game/Scenes/SocketDemoScene/SocketDemoScript.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SocketDemoScript : MonoBehaviour
+{
+    public TMP_InputField TextMeshProInputField;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SocketCommunication.Instance.Send(TextMeshProInputField.text);
+            TextMeshProInputField.text = string.Empty;
+            TextMeshProInputField.ActivateInputField();
+        }
+    }
+}
