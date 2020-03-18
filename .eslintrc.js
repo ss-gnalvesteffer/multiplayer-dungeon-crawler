50be2842e2ef9d6db5775effb6ee0a1f5b19ee8b
Broderick Young <bxyoung89@gmail.com>
2020-03-17 22:57:51 -0500

    linting (#3)
    see readme for more info.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"parser": "babel-eslint",
+	parserOptions: {
+		"ecmaVersion": 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	env: {
+		"browser": true,
+		"node": true,
+		"es6": true,
+	}
+};
--- game/client/src/components/button/index.js
+++ game/client/src/components/button/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './button.less';
 
 const Button = ({children, onClick}) => {
 	return (<button onClick={onClick} className={style['button']}>
 		{children}
 	</button>)
+};
+
+Button.propTypes = {
+	children: PropTypes.node.isRequired,
+	onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+	onClick: () => {},
 };
 
 export default Button;
