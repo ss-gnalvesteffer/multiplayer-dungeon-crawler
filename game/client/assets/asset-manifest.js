00be64ff72ded6b0a0813ead574e31828d31a671
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-03-22 23:01:44 -0500

    remove outdated asset references

--- game/client/assets/asset-manifest.js
+++ game/client/assets/asset-manifest.js
@@ -21,14 +21,6 @@ module.exports = {
     },
   },
   character: {
-    head_texture_path: 'assets/character/head.png',
-    torso_texture_path: 'assets/character/torso.png',
-    left_arm_texture_path: 'assets/character/left-arm.png',
-    left_hand_texture_path: 'assets/character/left-hand.png',
-    right_arm_texture_path: 'assets/character/right-arm.png',
-    right_hand_texture_path: 'assets/character/right-hand.png',
-    legs_texture_path: 'assets/character/legs.png',
-    left_foot_texture_path: 'assets/character/left-foot.png',
-    right_foot_texture_path: 'assets/character/right-foot.png',
+
   },
 };
