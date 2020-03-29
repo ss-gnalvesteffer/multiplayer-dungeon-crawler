6863c7e08dbc9eb80541a0b7efcafcc2cff93997
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-03-29 13:23:27 -0500

    remove blender project files to reduce file size in repo

---
+++ game/client/src/game/entities/character/index.js
@@ -0,0 +1,12 @@
+//import assetManifest from '../../../../assets/asset-manifest';
+import EntityBase from '../entity-base';
+
+export default class Character extends EntityBase {
+  constructor() {
+    super();
+  }
+
+  update() {
+
+  }
+}
